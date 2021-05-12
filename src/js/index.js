399346fb99ce776a8b05e4f262156415511e873e
StarBurst911 <71397969+StarBurst911@users.noreply.github.com>
2021-05-11 23:00:20 -0400

    working on getting data

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
-const key = a9cb170fcb7376800f05d8e3176d1cc1
-
 const query = async function () {
     try {
         const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
         const data = await response.json();
-        console.log(data);
+        data.results.forEach((tvshow) => {
+            DOMSelectors.grid.insertAdjacentHTML("beforeend", ''
+        });
     } catch (error) {
         console.log(error);
         alert("Something went wrong");
     }
 }
+query();
