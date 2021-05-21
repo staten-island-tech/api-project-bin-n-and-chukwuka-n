701991dacd81fafeae70aa8f0c1ede4d01bd08fc
StarBurst911 <71397969+StarBurst911@users.noreply.github.com>
2021-05-20 22:34:03 -0400

    working on displaying data 2

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,33 @@ const query = async function () {
     try {
         const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
         const data = await response.json();
-        data.results.forEach((tvshow) => {
-            DOMSelectors.grid.insertAdjacentHTML("beforeend", ''
-        });
+        console.log(data.results)
+        data.results.forEach((TVshows) => {
+            DOMSelectors.grid.insertAdjacentHTML("beforeend",
+            '<div class="TVshow-card">
+            <div class="TvShow-front">
+              <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nu6dcBfxr4VmOBj4k1S9r0r1MOW.jpg" 
+              alt=""
+            class="poster" />
+            </div>
+            <div class="Tvshow-back">
+              <h2 class="Tvshow-header">${TVshows.original_title}</h2>
+              <div class="score-box">
+                <p class="user-score">Community Score</p>
+                <p class="user-score">8.2</p>
+              </div>
+            </div>
+            <div class="release-date">
+              <p class="releasing-date">Released</p>
+              <p class="releasing-date">2015-10-09</p>
+            </div>
+            <div class="TVshow-genres">
+              <li class="TVshow-genre">Action and Adventure</li>
+              <li class="TVshow-genre">Comedy</li>
+              <li class="TVshow-genre">Sci-Fi</li>
+              <li class="TVshow-genre">Comedy</li>
+            </div>')
+        })
     } catch (error) {
         console.log(error);
         alert("Something went wrong");
