a71292cacda503f45149212596850f2e3cffed18
Chuks-maker <71624064+Chuks-maker@users.noreply.github.com>
2021-05-26 09:06:35 -0400

    j

--- src/js/search.js
+++ src/js/search.js
@@ -5,7 +5,7 @@ const listen= function () {
     DOMSelectors.searchForm.addEventListener("submit", function(e) {
         e.preventDefault();
         const searchParams = DOMSelectors.searchArea.nodeValue;
-        const searchQuery = try {
+        const searchQuery = async function(){try {
             const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
             const data = await response.json();
             console.log(data.results)
@@ -35,14 +35,14 @@ const listen= function () {
                   <li class="TVshow-genre">Sci-Fi</li>
                   <li class="TVshow-genre">Comedy</li>
                 </div>')
-              </div>
-            })
-        } `
-        catch (error) {
+              </div>`
+            )
+        })
+      }
+      catch (error) {
           console.log(error);
           alert("Something went wrong");
-        }
-    });
-}
-
+        };
+      }})}
+    
 SearchQuery();
