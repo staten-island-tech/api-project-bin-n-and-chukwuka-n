f9f042ca1a53e4fb674ff672102f9d2963e6ab20
mr. whalen <michael.whalen@sitechhs.com>
2021-06-01 12:32:51 -0400

    Chukes and Bin bad

--- src/js/search.js
+++ src/js/search.js
@@ -1,18 +1,20 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "genre";
-console.log(DOMSelectors.searchForm)
+console.log(DOMSelectors.searchForm);
 const listen = function () {
-  console.log("listen")
+  console.log("listen");
   DOMSelectors.searchForm.addEventListener("submit", function (e) {
     console.log("DOMSelectors.searchForm");
     e.preventDefault();
     const searchParams = DOMSelectors.searchArea.value;
-    console.log(searchParams)
+    console.log(searchParams);
     const searchQuery = async function () {
       try {
-        const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
+        const response = await fetch(
+          "https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate"
+        );
         const data = await response.json();
-        console.log(data.results)
+        console.log(data.results);
         data.results.forEach((TVshows) => {
           DOMSelectors.grid.insertAdjacentHTML(
             "beforeend",
@@ -40,17 +42,15 @@ const listen = function () {
                   <li class="TVshow-genre">Comedy</li>
                 </div>')
               </div>`
-          )
-        })
-      }
-      catch (error) {
+          );
+        });
+      } catch (error) {
         console.log(error);
         alert("Something went wrong");
-      };
-
-    }
+      }
+    };
     searchQuery();
-  })
-}
+  });
+};
 
 listen();
