4eada276433caf388ab683f50a8091f57a2700d4
Chuks-maker <71624064+Chuks-maker@users.noreply.github.com>
2021-06-03 14:56:05 -0400

    j

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { DOMSelectors } from "./DOM";
 const query = async function () {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
+        const response = await fetch('https://api.themoviedb.org/3/discover/tv?api_key=a9cb170fcb7376800f05d8e3176d1cc1&language=en-US&sort_by=vote_average.desc&page=1&timezone=America%2FNew_York&vote_count.gte=7000&include_null_first_air_dates=false&with_watch_monetization_types=flatrate');
         const data = await response.json();
         console.log(data.results)
         data.results.forEach((TVshows) => {
@@ -13,7 +13,7 @@ const query = async function () {
                 class="poster" />
             </div>
             <div class="tv-card-back">
-              <h2 class="tv-card-header">${tv.original_name}</h2>
+              <h2 class="tv-card-header">The Tales of Miraculous LadyBug</h2>
               <div class="scorebox">
                 <p class="user-score">Community Score</p>
                 <p class="user-score">7.9</p>
